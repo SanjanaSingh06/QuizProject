7588ea90e45d8f2fbe4e19cb151027158c167253
SanjanaSingh06 <161630838+SanjanaSingh06@users.noreply.github.com>
2024-04-20 22:08:25 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,62 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the current Prime Minister of India?",
+    answer: "Narendra Modi",
+    options: [
+      "Narendra Modi",
+      "Rahul Gandhi",
+      "Amit Shahr",
+      "Manmohan Singh"
+      
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which Indian state recently conducted elections resulting in a change of government?",
+    answer: " Goa",
+    options: [
+        "Uttar Pradesh",
+        "Maharashtra",
+        "Punjab",
+        "Goa"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the name of the recently launched initiative by the Indian government to promote digital payments?",
+    answer: "Cashless India",
+    options: [
+        "Digital India",
+        "Cashless India",
+        "Paytm India",
+        "UPI India"
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who won the 2023 ICC Men's Cricket World Cup held in India?",
+    answer: "Australia",
+    options: [
+    "Australia",
+    "England",
+    "India",
+    "New Zealand"
+      
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which Indian city recently topped the list of the world's most polluted cities according to a report by the World Health Organization (WHO)?",
+    answer: "Friday",
+    options: [
+        "Delhi ",
+        "Mumbai",
+        "Kolkata",
+        "Chennai"
+    ]
+  },
+  
+];
